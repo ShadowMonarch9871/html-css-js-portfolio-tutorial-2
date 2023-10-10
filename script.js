5c77bb030d02994126d7a09e9f727527aad2ab97
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2023-10-10 19:49:20 +0530

    message

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const btn_trying = document.getElementsByClassName("btn_trying")[0];
+const body = document.body;
+
+btn_trying.addEventListener("click", () => {
+  const randomColor = getRandomColor();
+  body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
